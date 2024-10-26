fccd1f037e86f5662e4fc4bbeaec89e580cdcb5d
LeonardoNXT <Leonardoffice12@gmail.com>
2024-10-26 13:17:54 -0300

    Nine Update

---
+++ pages/Page2/script.js
@@ -0,0 +1,16 @@
+const lastOne = document.querySelector('.hidden')
+
+const myObserver = new IntersectionObserver(entries => {
+    console.log(entries)
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        }
+        else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+elements.forEach(el => myObserver.observe(el))
