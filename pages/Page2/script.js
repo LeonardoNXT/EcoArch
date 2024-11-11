8ccf76ac4c9cc873b34c8947a64dca0456a41285
LeonardoNXT <Leonardoffice12@gmail.com>
2024-11-11 01:00:05 -0300

    Eleven UPDATE I

--- pages/Page2/script.js
+++ pages/Page2/script.js
@@ -64,7 +64,6 @@ if (elementoParaObservar) {
 
 
 const myObserver2 = new IntersectionObserver(entries => {
-    console.log(entries);
     entries.forEach(entry => {
         const gradient2 = document.querySelector('.bottom-gradient')
         const body = document.querySelector('body')
@@ -86,6 +85,28 @@ const elementoParaObservar1 = document.querySelector('.overlay');
 if (elementoParaObservar1) {
     myObserver2.observe(elementoParaObservar1);
 }
+const myObserver3 = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        const gradient2 = document.querySelector('#bottom-gradient-3')
+        const body = document.querySelector('body')
+        console.log(entry.isIntersecting);      
+        if (entry.isIntersecting) {
+            gradient2.style.opacity = '1'
+            body.style.backgroundColor = 'white'
+        }
+        else{
+            gradient2.style.opacity = '0'
+        }
+    });
+});
+
+// Seleciona o elemento pelo `id` que você quer observar
+const elementoParaObservar2 = document.querySelector('.overlay-config-2');
+
+// Verifica se o elemento existe e então começa a observar
+if (elementoParaObservar2) {
+    myObserver3.observe(elementoParaObservar2);
+}
 
 
 
@@ -102,10 +123,14 @@ function openModal() {
     const janela5 = document.querySelector('#fourth')
     const janela6 = document.querySelector('#fifth')
     const janela7 = document.querySelector('#sixth')
+    const janela8 = document.querySelector('#seventh')
+    const janela9 = document.querySelector('#seventh-2')
     const targetDiv = document.querySelector('#part-1');
 
     janela1.style.display = 'flex'
     janela1.style.opacity = '1'
+    janela1.style.animation = 'opacity 1s ease-in-out'
+
     janela2.style.display = 'flex'
     janela2.style.opacity = '1'
     janela3.style.display = 'flex'
@@ -118,6 +143,11 @@ function openModal() {
     janela6.style.opacity = '1'
     janela7.style.display = 'flex'
     janela7.style.opacity = '1'
+    janela8.style.display = 'flex'
+    janela8.style.opacity = '1'
+    janela9.style.display = 'flex'
+    janela9.style.opacity = '1'
+
 
     if (targetDiv) {
         targetDiv.scrollIntoView({ behavior: 'smooth' });
@@ -128,10 +158,12 @@ function openModal() {
         entries.forEach(entry => {
             const gradient2 = document.querySelector('#bottom-gradient-2')
             const gradient3 = document.querySelector('#top-gradient-2')
+            const body = document.querySelector('body')
             
             if (entry.isIntersecting) {
                 gradient2.style.opacity = '1'
                 gradient3.style.opacity = '1'
+                body.style.backgroundColor = 'black'
             }
             else{
                 gradient2.style.opacity = '0'
